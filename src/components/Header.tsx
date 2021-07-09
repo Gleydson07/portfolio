95c24c3b4d85869122f6194a3f24e9f0da7418a8
Gleydson07 <gassantos.dev@gmail.com>
2021-07-09 00:45:49 -0300

    change color on mouse hover header social media

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -79,19 +79,39 @@ export function Header(){
                     gap="6"
                 >
                     <Link href="https://www.linkedin.com/in/gleydson07/" isExternal >
-                        <Icon as={SiLinkedin} fontSize="24"/>
+                        <Icon 
+                            as={SiLinkedin} 
+                            fontSize="24"
+                            transition="color 0.3s"
+                            _hover={{color: "cyan.600"}}
+                        />
                     </Link>
 
                     <Link href="https://github.com/Gleydson07" isExternal >
-                        <Icon as={SiGithub} fontSize="24"/>
+                        <Icon 
+                            as={SiGithub} 
+                            fontSize="24"
+                            transition="color 0.3s"
+                            _hover={{color: "cyan.600"}}
+                        />
                     </Link>
 
                     <Link href="https://api.whatsapp.com/send?phone=558281114246&text=Ol%C3%A1!!%20%F0%9F%98%80" isExternal >
-                        <Icon as={SiWhatsapp} fontSize="24"/>
+                        <Icon 
+                            as={SiWhatsapp} 
+                            fontSize="24"
+                            transition="color 0.3s"
+                            _hover={{color: "cyan.600"}}
+                        />
                     </Link>
 
                     <Link href="https://mail.google.com/mail/u/0/?fs=1&to=gassantos.dev@gmail.com&su=Ol%C3%A1!%20fala%20aqui%20qual%20assunto%20legal%20que%20vamos%20tratar.&body=Aqui%20voc%C3%AA%20pode%20descrever%20melhor%20sua%20mensagem,%20pode%20ser%20uma%20pergunta,%20uma%20sugest%C3%A3o,%20um%20convite%20para%20um%20caf%C3%A9%20%F0%9F%98%80&tf=cm" isExternal >
-                        <Icon as={SiGmail} fontSize="24"/>
+                        <Icon 
+                            as={SiGmail} 
+                            fontSize="24"
+                            transition="color 0.3s"
+                            _hover={{color: "cyan.600"}}
+                        />
                     </Link>
                 </Grid>
             </Flex>
