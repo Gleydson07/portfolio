fa65dede5c431222367f8632e9254189ad84bb6f
Gleydson07 <gassantos.dev@gmail.com>
2021-07-12 10:14:26 -0300

    Welcome box to home

--- src/components/Header/SocialMedia.tsx
+++ src/components/Header/SocialMedia.tsx
@@ -1,4 +1,4 @@
-import { Flex, Text, Grid, Icon, Link } from '@chakra-ui/react'
+import { Flex, Text, Grid, Icon, Link, GridItem } from '@chakra-ui/react'
 
 import { SiLinkedin, SiGithub, SiWhatsapp, SiGmail } from 'react-icons/si'
 
@@ -6,43 +6,55 @@ export function SocialMedia(){
     return (
         <Grid 
             display="flex"
-            gap="6"
+            templateColumns="repeat(4, 1fr)"
+            gap="5"
+            alignItems="center"
+            justifyContent="center"
         >
-            <Link href="https://www.linkedin.com/in/gsantosdev/" isExternal >
-                <Icon 
-                    as={SiLinkedin} 
-                    fontSize="24"
-                    transition="color 0.3s"
-                    _hover={{color: "cyan.600"}}
-                />
-            </Link>
+            <GridItem>
+                <Link href="https://www.linkedin.com/in/gsantosdev/" isExternal >
+                    <Icon 
+                        as={SiLinkedin} 
+                        fontSize="24"
+                        transition="color 0.3s"
+                        _hover={{color: "cyan.600"}}
+                    />
+                </Link>
+            </GridItem>
 
-            <Link href="https://github.com/Gleydson07" isExternal >
-                <Icon 
-                    as={SiGithub} 
-                    fontSize="24"
-                    transition="color 0.3s"
-                    _hover={{color: "cyan.600"}}
-                />
-            </Link>
+            <GridItem>
+                <Link href="https://github.com/Gleydson07" isExternal >
+                    <Icon 
+                        as={SiGithub} 
+                        fontSize="24"
+                        transition="color 0.3s"
+                        _hover={{color: "cyan.600"}}
+                    />
+                </Link>
+            </GridItem>
 
-            <Link href="https://api.whatsapp.com/send?phone=558281114246&text=Ol%C3%A1!!%20%F0%9F%98%80" isExternal >
-                <Icon 
-                    as={SiWhatsapp} 
-                    fontSize="24"
-                    transition="color 0.3s"
-                    _hover={{color: "cyan.600"}}
-                />
-            </Link>
+            <GridItem>
+                <Link href="https://api.whatsapp.com/send?phone=558281114246&text=Ol%C3%A1!!%20%F0%9F%98%80" isExternal >
+                    <Icon 
+                        as={SiWhatsapp} 
+                        fontSize="24"
+                        transition="color 0.3s"
+                        _hover={{color: "cyan.600"}}
+                    />
+                </Link>
+            </GridItem>
 
-            <Link href="https://mail.google.com/mail/u/0/?fs=1&to=gassantos.dev@gmail.com&su=Ol%C3%A1!%20fala%20aqui%20qual%20assunto%20legal%20que%20vamos%20tratar.&body=Aqui%20voc%C3%AA%20pode%20descrever%20melhor%20sua%20mensagem,%20pode%20ser%20uma%20pergunta,%20uma%20sugest%C3%A3o,%20um%20convite%20para%20um%20caf%C3%A9%20%F0%9F%98%80&tf=cm" isExternal >
-                <Icon 
-                    as={SiGmail} 
-                    fontSize="24"
-                    transition="color 0.3s"
-                    _hover={{color: "cyan.600"}}
-                />
-            </Link>
+            <GridItem>
+                <Link href="https://mail.google.com/mail/u/0/?fs=1&to=gassantos.dev@gmail.com&su=Ol%C3%A1!%20fala%20aqui%20qual%20assunto%20legal%20que%20vamos%20tratar.&body=Aqui%20voc%C3%AA%20pode%20descrever%20melhor%20sua%20mensagem,%20pode%20ser%20uma%20pergunta,%20uma%20sugest%C3%A3o,%20um%20convite%20para%20um%20caf%C3%A9%20%F0%9F%98%80&tf=cm" isExternal >
+                    <Icon 
+                        as={SiGmail} 
+                        fontSize="24"
+                        transition="color 0.3s"
+                        _hover={{color: "cyan.600"}}
+                    />
+                </Link>
+            </GridItem>
+
         </Grid>
     )
 }
--- src/components/WelcomeBox.tsx
+++ src/components/WelcomeBox.tsx
@@ -3,11 +3,17 @@ import { SiLinkedin, SiGithub, SiWhatsapp, SiGmail } from 'react-icons/si'
 
 import meMobileImg from '../images/me/me-mobile.jpeg'
 import Typed from 'react-typed';
+import { SocialMedia } from './Header/SocialMedia';
 
 export function WelcomeBox(){
     const isWideVersion = useBreakpointValue({
         base: false,
         lg: true
+    })
+
+    const isMiddleVersion = useBreakpointValue({
+        base: false,
+        md: true
     })
 
     return (
@@ -23,6 +29,7 @@ export function WelcomeBox(){
                 <Flex
                     w="100%"
                     mb="1rem"
+                    alignItems="center"
                 >
                     <Image 
                         className="animate__animated animate__slower animate__fadeInDown"
@@ -75,6 +82,7 @@ export function WelcomeBox(){
                 textAlign="left"
                 h="2.62rem"
                 mt="0.25rem"
+                mb="2rem"
             >
                 <>
                     <Typed
@@ -112,48 +120,7 @@ export function WelcomeBox(){
                     </Text>
                 </>
             ):(
-                <Grid 
-                    display="flex"
-                    gap="4"
-                    mt="1.5rem"
-                    justifyContent="center"
-                >
-                    <Link href="https://www.linkedin.com/in/gleydson07/" isExternal >
-                        <Icon 
-                            as={SiLinkedin} 
-                            fontSize="24"
-                            transition="color 0.3s"
-                            _hover={{color: "cyan.600"}}
-                        />
-                    </Link>
-
-                    <Link href="https://github.com/Gleydson07" isExternal >
-                        <Icon 
-                            as={SiGithub} 
-                            fontSize="24"
-                            transition="color 0.3s"
-                            _hover={{color: "cyan.600"}}
-                        />
-                    </Link>
-
-                    <Link href="https://api.whatsapp.com/send?phone=558281114246&text=Ol%C3%A1!!%20%F0%9F%98%80" isExternal >
-                        <Icon 
-                            as={SiWhatsapp} 
-                            fontSize="24"
-                            transition="color 0.3s"
-                            _hover={{color: "cyan.600"}}
-                        />
-                    </Link>
-
-                    <Link href="https://mail.google.com/mail/u/0/?fs=1&to=gassantos.dev@gmail.com&su=Ol%C3%A1!%20fala%20aqui%20qual%20assunto%20legal%20que%20vamos%20tratar.&body=Aqui%20voc%C3%AA%20pode%20descrever%20melhor%20sua%20mensagem,%20pode%20ser%20uma%20pergunta,%20uma%20sugest%C3%A3o,%20um%20convite%20para%20um%20caf%C3%A9%20%F0%9F%98%80&tf=cm" isExternal >
-                        <Icon 
-                            as={SiGmail} 
-                            fontSize="24"
-                            transition="color 0.3s"
-                            _hover={{color: "cyan.600"}}
-                        />
-                    </Link>
-                </Grid>
+                !isMiddleVersion ? <SocialMedia/> : ''
             )}
         </Box>
 
